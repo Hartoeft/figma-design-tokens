6ec7bdd578a4a04bc6d2f3c685efb5e421ded876
Christian <cha@impact.dk>
2022-07-22 16:57:11 +0200

    Added TODO for custom fluid font size

--- src/models/config.model.ts
+++ src/models/config.model.ts
@@ -16,11 +16,16 @@ export interface Config {
     figmaFileId: string;
     figmaTeamId: string;
     figmaToken?: string; // Add your personal figma api token or add it to an .env file
+    /**
+     * Optional: If you want to use a fluid font, you can set the container size
+     */
     fluidFont?: {
         /**
-         * Add the max width for your content container e.g. 1440 (px)
+         * Add the max width for your content container e.g. 1440 (px),
+         * If you add a value then the font-size will be returned as a css clamp() value.
          */
         desktopContainerSize: number;
+        // customFluidFunction: Function; // TODO: Add support for custom fluid function
     };
     /**
      * Choose where to output files. Defaults to ./dist
